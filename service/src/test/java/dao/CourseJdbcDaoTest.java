c6d0ff520e69cf705c63ccde513e41f9b3256cdc
Ikiselev7 <ikiselev7@gmail.com>
2017-11-06 21:28:10 +0300

    CourseJdbcDaoTest

--- service/src/test/java/dao/CourseJdbcDaoTest.java
+++ service/src/test/java/dao/CourseJdbcDaoTest.java
@@ -1,16 +1,36 @@
 package dao;
 
+import context.Course;
+import context.CourseFactory;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.util.List;
+import java.util.Properties;
 
 import static org.junit.Assert.*;
 
 public class CourseJdbcDaoTest {
-    private CourseJdbcDao courseJdbcDao;
     private JdbcDaoFactory jdbcDaoFactory;
+    private CourseJdbcDao courseJdbcDao;
 
     @Before
     public void setUp() throws Exception {
+        Properties properties = new Properties();
+        properties.load(JdbcTemplate.class.getResourceAsStream("/databaseTest.properties"));
+
+        // create data source
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        String className = properties.getProperty("driverClassName");
+        dataSource.setDriverClassName(className);
+        dataSource.setUrl(properties.getProperty("url"));
+        dataSource.setConnectionProperties(properties);
+
+        jdbcDaoFactory = new JdbcDaoFactoryImpl(dataSource);
+        courseJdbcDao = jdbcDaoFactory.getCourseDao();
+
     }
 
     @Test
@@ -27,6 +47,16 @@ public class CourseJdbcDaoTest {
 
     @Test
     public void create() throws Exception {
+        CourseJdbcDao courseJdbcDao = jdbcDaoFactory.getCourseDao();
+        CourseFactory courseFactory = new CourseFactory();
+
+        Course target = courseFactory.newInstance(0,"JavaCore", null, null);
+        int id = courseJdbcDao.create(target);
+        List<Course> courses = courseJdbcDao.list();
+        Course course = courseJdbcDao.read(id);
+
+        assertTrue(target.equals(course));
+        assertTrue(courses.contains(target));
     }
 
     @Test
