6d9e8d687645cc0d4aa4814c152ddeafbf9a1668
denklewer <denklewer@gmail.com>
2017-11-12 19:21:09 +0300

    local commit

--- service/src/main/java/appconfig/AppConfig.java
+++ service/src/main/java/appconfig/AppConfig.java
@@ -26,6 +26,7 @@ public class AppConfig {
     private Environment environment;
 
 
+
     @Bean
     public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
