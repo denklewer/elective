2c9b93cf0cb149be1631c38d22f7d1d94ec7c838
Ikiselev7 <ikiselev7@gmail.com>
2017-11-13 17:07:46 +0300

    fix

--- web/src/main/java/epam_team1/web/WebApplication.java
+++ web/src/main/java/epam_team1/web/WebApplication.java
@@ -5,13 +5,21 @@ import epam_team1.service.ServiceApplication;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.convert.support.ConfigurableConversionService;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MissingRequiredPropertiesException;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.StandardEnvironment;
 
+import java.util.Map;
 
 @SpringBootApplication
 public class WebApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder()
                 .bannerMode(Banner.Mode.CONSOLE)
+                //.environment(new StandardEnvironment().)
                 .sources(ServiceApplication.class, WebApplication.class)
                 .run(args);
     }
