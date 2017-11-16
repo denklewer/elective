688631ab0be10d2b81732ce32fcf1e42d605f552
denklewer <denklewer@gmail.com>
2017-11-16 15:19:14 +0300

    config

--- web/src/main/java/epam_team1/web/config/SecurityConfig.java
+++ web/src/main/java/epam_team1/web/config/SecurityConfig.java
@@ -34,9 +34,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .jdbcAuthentication()
                 .dataSource(dataSource)
                 .usersByUsernameQuery("SELECT Login, Password, 1 FROM User WHERE Login = ?")
-                 .authoritiesByUsernameQuery("select Login, 'ROLE_ADMIN' from User where Login=?");
+                .authoritiesByUsernameQuery("select Login, 'ROLE_ADMIN' from User where Login=?");
     }
 
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withUsername("user").password("password").roles("USER").build());
+//        return manager;
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -54,9 +61,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable();
 
+        // http.formLogin().successHandler(authenticationSuccessHandler);
 
+        // чтобы войти в личный кабинет.
+     /*   http.formLogin()
+                // указываем страницу с формой логина
+                .loginPage("/login")
+                // указываем action с формы логина
+                //.loginProcessingUrl("/j_spring_security_check")
+                // указываем URL при неудачном логине
+                .failureUrl("/login?error")
+                // Указываем параметры логина и пароля с формы логина
+                .usernameParameter("j_username")
+                .passwordParameter("j_password")
+                // даем доступ к форме логина всем
+                .permitAll();
+                */
+
+        // чтобы выйти из личного кабинета.
+      /*  http.logout()
+                // разрешаем делать логаут всем
+                .permitAll()
+                // указываем URL логаута
+                .logoutUrl("/logout")
+                // указываем URL при удачном логауте
+                .logoutSuccessUrl("/login?logout")
+                // делаем не валидной текущую сессию
+                .invalidateHttpSession(true);*/
     }
 
+
+//        http.logout()
+//
+//                .logoutUrl("/my/logout")
+//                .logoutSuccessUrl("/my/index")
+//                .logoutSuccessHandler(logoutSuccessHandler)
+//                .invalidateHttpSession(true)
+//                .addLogoutHandler(logoutHandler)
+//                .deleteCookies(cookieNamesToClear)
+//                .and();
+
+    // эти запросы доступны для всех пользователей
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
