36d4d76c4ab50434b5ce496da721bc072c8897dc
Ikiselev7 <ikiselev7@gmail.com>
2017-11-20 12:56:19 +0300

    last working version

--- web/src/main/resources/static/js/tables/register.js
+++ web/src/main/resources/static/js/tables/register.js
@@ -1,5 +1,64 @@
 document.getElementById("register").onclick = function() {register()};
 
+function getLang(){
+    var lang = $.cookie('lang');
+    console.log(lang);
+
+    if(lang == null){
+        return 'en_EN';
+    }
+
+    return lang;
+}
+
+var langProp = {
+    name: 'Messeges',
+    path: '/',
+    mode: 'both',
+    language: getLang(),
+    callback: function() {
+
+
+        $("#lang").text($.i18n.prop('lang'));
+        $("#inputFirstName").text($.i18n.prop('firstname'));
+        document.getElementById("inputFirstName").setAttribute("placeholder", $.i18n.prop('firstname'));
+        $("#inputLastName").text($.i18n.prop('lastname'));
+        document.getElementById("inputLastName").setAttribute("placeholder", $.i18n.prop('lastname'));
+        $("#inputLogin").text($.i18n.prop('login'));
+        document.getElementById("inputLogin").setAttribute("placeholder", $.i18n.prop('login'));
+        $("#inputEmail").text($.i18n.prop('email'));
+        document.getElementById("inputEmail").setAttribute("placeholder", $.i18n.prop('email'));
+        $("#inputPassword").text($.i18n.prop('password'));
+        document.getElementById("inputPassword").setAttribute("placeholder", $.i18n.prop('password'));
+        $("#regWelcome").text($.i18n.prop('regWelcome'));
+        $("#regBut").text($.i18n.prop('registerBut'));
+
+    }
+};
+
+
+$.i18n.properties(langProp);
+
+document.getElementById("lang").addEventListener("click", function () {
+    var lang = $.cookie('lang');
+    console.log(lang);
+
+    if(lang == null){
+        lang = 'ru_RU';
+    }else if(lang == 'ru_RU'){
+        lang = 'en_EN';
+    }else{
+        lang = 'ru_RU';
+    }
+    $.cookie('lang', lang, {
+        expires: 5,
+        path: '/',
+    });
+    langProp.language = lang;
+    $.i18n.properties(langProp);
+
+});
+
 function register() {
     console.log("clicked");
     var user = {
